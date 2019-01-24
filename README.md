f7cea37ca53e584a1fa7a0a565062fe04354b865
Nombre Apellido <jvaldivia@softweb.pe>
2019-01-24 17:21:41 -0500

    Diferentes outputs

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 + https://webpack.js.org/guides/getting-started/
 + https://webpack.js.org/concepts/mode/
++ https://stackoverflow.com/questions/35903246/how-to-create-multiple-output-paths-in-webpack-config
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,32 @@
 const path = require('path');
 
-module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-     rules: [
-       {
-         test: /\.css$/,
-         use: [
-           'style-loader',
-           'css-loader'
-         ]
-       }
-     ]
-   }
+var config = {
+    // TODO: Add common Configuration
+    module: {},
 };
+
+var fooConfig = Object.assign({}, config, {
+    name: "a",
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    }
+});
+
+// Array of Configurations
+module.exports = [
+    fooConfig,        
+];

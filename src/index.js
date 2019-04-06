bd41e12a9972d8ddd16423df6038c47075ba87ec
Pepe Valdivia <jvaldivia@softweb.pe>
2019-04-05 20:31:28 -0500

    Webpack como modos de ambiente

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import District from './models/district';
 
 var x = new District();
 
-alert("XD 11");
+alert("XD 112334");
 
 console.log(x);
--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,9 @@ var config = {
     module: {},
 };
 
-var fooConfig = Object.assign({}, config, {
+var devConfig = Object.assign({}, config, {
     name: 'a',
-    mode: 'production',
+    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: '[name].js',
@@ -72,7 +72,72 @@ var fooConfig = Object.assign({}, config, {
     ]
 });
 
+var devConfig = Object.assign({}, config, {
+    name: 'a',
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'public/dist')
+    },
+    watch: false, // autoreload ante cambios
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            // you can specify a publicPath here
+                            // by default it use publicPath in webpackOptions.output
+                            publicPath: '../'
+                        }
+                    },
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ]
+            }
+        ],
+    },
+    devServer: {
+        host: '0.0.0.0',
+        port: 9090,
+        contentBase: [
+            path.join(__dirname, './public'),
+            path.join(__dirname, './src'),
+        ],
+        compress: true,
+        watchContentBase: true,
+        hot: true,
+        inline:true,
+        allowedHosts: [
+            'host.com',
+            '*',
+        ],
+        headers: {
+            'Server': 'Ubuntu'
+        }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        }),
+        // new webpack.HotModuleReplacementPlugin(),
+    ]
+});
+
 // Array of Configurations
 module.exports = [
-    fooConfig,
+    devConfig,
 ];
